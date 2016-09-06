a857d0bb70aeee865c5a6e67a472db2f038e7da5
Yuan Yiyang <yuan3y@users.noreply.github.com>
2016-09-07 00:40:25 +0800

    put into a timeslots class
    restored "no available pitches" output from the original version

--- sportssearch.py
+++ sportssearch.py
@@ -10,37 +10,48 @@ from datetime import datetime, timedelta
 from pytz import timezone
 import sys
 
-def print0(val):
-    if val and len(val):
-        for v in val:
-            print("{0}".format(v))
+class timeslots(object):
+    def __init__(self):
+        self.results = []
+
+    def print0(self, val):
+        if val and len(val):
+            for v in val:
+                print("{0}".format(v))
+                self.results.append(v)
+
+    def search(self):
+        start=time()
+        if len(sys.argv) == 1:
+            start_time = datetime.now(timezone('Asia/Singapore')).replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
+            end_time = datetime.now(timezone('Asia/Singapore')).replace(minute=0, second=0, microsecond=0) + timedelta(hours=2)
+            check_date = start_time.strftime("%Y-%m-%d")
+            check_time_start = start_time.strftime("%R")
+            check_time_end = end_time.strftime("%R")
+
+        else:
+            check_date = sys.argv[1]
+            check_time_start = sys.argv[2]
+            check_time_end = sys.argv[3]
+
+        concurrent_processes = mp.cpu_count()
+        pool = mp.Pool(processes=concurrent_processes)
+
+        [pool.apply_async(get_offside_availability, args=(check_date, check_time_start, check_time_end), callback=self.print0)    ,
+        pool.apply_async(get_kovansports_availability, args=(check_date, check_time_start, check_time_end), callback=self.print0) ,
+        pool.apply_async(get_zionsports_availability, args=(check_date, check_time_start, check_time_end), callback=self.print0)  ,
+        pool.apply_async(get_kallangcage_availability, args=(check_date, check_time_start, check_time_end), callback=self.print0) ,
+        pool.apply_async(get_bttimahcage_availability, args=(check_date, check_time_start, check_time_end), callback=self.print0) ,
+        pool.apply_async(get_hyfa_availability, args=(check_date, check_time_start, check_time_end), callback=self.print0),
+        ]
+        pool.close()
+        pool.join()
+        if not self.results:
+            print 'No available pitches found.'
+
+        print '%.2f sec.' %(time()-start)
+
 
 if __name__ == '__main__':
-
-    start=time()
-    if len(sys.argv) == 1:
-        start_time = datetime.now(timezone('Asia/Singapore')).replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
-        end_time = datetime.now(timezone('Asia/Singapore')).replace(minute=0, second=0, microsecond=0) + timedelta(hours=2)
-        check_date = start_time.strftime("%Y-%m-%d")
-        check_time_start = start_time.strftime("%R")
-        check_time_end = end_time.strftime("%R")
-
-    else:
-        check_date = sys.argv[1]
-        check_time_start = sys.argv[2]
-        check_time_end = sys.argv[3]
-
-    concurrent_processes = mp.cpu_count()
-    pool = mp.Pool(processes=concurrent_processes)
-
-    [pool.apply_async(get_offside_availability, args=(check_date, check_time_start, check_time_end), callback=print0)    ,
-    pool.apply_async(get_kovansports_availability, args=(check_date, check_time_start, check_time_end), callback=print0) ,
-    pool.apply_async(get_zionsports_availability, args=(check_date, check_time_start, check_time_end), callback=print0)  ,
-    pool.apply_async(get_kallangcage_availability, args=(check_date, check_time_start, check_time_end), callback=print0) ,
-    pool.apply_async(get_bttimahcage_availability, args=(check_date, check_time_start, check_time_end), callback=print0) ,
-    pool.apply_async(get_hyfa_availability, args=(check_date, check_time_start, check_time_end), callback=print0),
-    ]
-    pool.close()
-    pool.join()
-
-    print '%.2f sec.' %(time()-start)
+    t = timeslots()
+    t.search()
